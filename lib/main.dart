d61a116689c33356515c0f99e3df377fda766884
Shark <almo5tare3123@gmail.com>
2024-04-09 14:50:25 +0200

    Update app icons and labels in Android and iOS;

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:storeapp/core/app/env_variables.dart';
 
 import 'store_app.dart';
 
