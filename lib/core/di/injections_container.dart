1ea8c23c17bb3bf0b5f2d21950bb7f16d22628da
Shark <almo5tare3123@gmail.com>
2024-04-22 23:38:25 +0200

    "Added Dio and Retrofit dependencies, updated InjectionsContainer and SharedKeys"
    This commit adds the Dio and Retrofit dependencies to the project, and makes changes to the InjectionsContainer and SharedKeys classes.

--- lib/core/di/injections_container.dart
+++ lib/core/di/injections_container.dart
@@ -1,5 +1,7 @@
 import 'package:get_it/get_it.dart';
 import 'package:storeapp/core/app/app_cubite/app_cubit.dart';
+import 'package:storeapp/core/services/graphql/api_service.dart';
+import 'package:storeapp/core/services/graphql/dio_factory.dart';
 
 final sl = GetIt.instance;
 Future<void> setupInjector() async {
@@ -9,6 +11,10 @@ Future<void> setupInjector() async {
   //usecase
 }
 
+
 Future<void> _initCore() async {
-  sl.registerFactory(AppCubit.new);
+final dio=DioFactory.getdio();
+
+
+  sl..registerFactory(AppCubit.new)..registerLazySingleton<ApiService>(() =>ApiService(dio));
 }
---
+++ lib/core/services/graphql/dio_factory.dart
@@ -0,0 +1,35 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_localization/flutter_localization.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+import 'package:storeapp/core/services/pref_keys.dart';
+import 'package:storeapp/core/services/shared_pref.dart';
+
+class DioFactory {
+  DioFactory._();
+  static Dio? dio;
+  static Dio getdio() {
+    const timeOut = Duration(seconds: 30);
+    if (dio == null) {
+      dio = Dio();
+      dio!
+        ..options.connectTimeout = timeOut
+        ..options.receiveTimeout = timeOut
+        ..options.headers['Authorization'] =
+            'Bearer ${SharedPref().getString(SharedKeys.accessToken)}';
+      debugPrint(
+          "{USER TOKEN }=======> ${SharedPref().getString(SharedKeys.accessToken) ?? 'NULL TOKEN'}");
+      addDioIntercepter();
+      return dio!;
+    } else {
+      return dio!;
+    }
+  }
+
+  static void addDioIntercepter() {
+    dio?.interceptors.add(PrettyDioLogger(
+      request: false,
+      compact: false,
+    ));
+  }
+}
